8cd6c870d38bde272cf1edd81f364ed56b2e4713
Eluriel <51481372+Eluriel@users.noreply.github.com>
2024-05-29 19:26:30 +0200

    Add Neopixel library

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
+#define NEOPIXEL_PIN 21
+#define NUMPIXELS 3
+
+Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
+int dir = 1;
+int bright = 0;
 // put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pixels.begin();
+  randomSeed(analogRead(0));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  pixels.clear();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for(int i=0; i<NUMPIXELS; i++) {
+
+  uint32_t blueFade = pixels.ColorHSV(43650,255,bright); //set blueFade color to variable bright
+  pixels.fill(blueFade);                                 //fill strip with color blueFade
+  pixels.show();                                         //send data to strip
+  bright = bright + dir;                                //change bright by +1 or -1
+  if(bright > 100 || bright < 1) dir = dir * -1;        //set dir to +1 or -1
+  delay(1000); 
+
+  int sensorValue = analogRead(1);
+  Serial.println(sensorValue);
+  }
 }
