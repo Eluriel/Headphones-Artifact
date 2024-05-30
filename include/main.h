7fb2ce5a079f19c36d506fea2dbbe66433d74a35
Eluriel <51481372+Eluriel@users.noreply.github.com>
2024-05-30 17:52:56 +0200

    Add sound mode and solid mode
    Change from adafruit neopixel to fastled
    
    Also use OneButton library for user interactions

---
+++ include/main.h
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+#include <FastLED.h>        // https://github.com/FastLED/FastLED
+#include <OneButton.h>
+
+FASTLED_USING_NAMESPACE
+
+// Input / Output
+#define BUTTON_PIN 4
+#define NEOPIXEL_PIN 6
+#define ENVELOPE_PIN A4 // Envelope Pin of the Sparkfun Sound Detector Module
+
+// Application constants
+#define NUM_LEDS 3
+#define BRIGHTNESS      220 // Min: 0, Max: 255
+#define SATURATION      255 // Min: 0, Max: 255
+#define MIN_VAL          10 // Min: 0, Max: 75
+#define MAX_VAL         150 // Min: 75, Max: 750
+#define HUE_INIT         HUE_AQUA // < 255
+#define HUE_CHANGE       32 // < 255
+#define ALPHA            0.1 // Smoothing factor (0 < alpha <= 1)
+
+enum LedModes {
+  SOUND_MODE = 0,
+  SOLID_MODE = 1,
+  FADE_MODE = 2,
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,97 @@
-#include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
+#include "main.h"
 
-#define NEOPIXEL_PIN 21
-#define NUMPIXELS 3
+CRGB leds[NUM_LEDS];
+int mode = 0;
+int currentHue = HUE_INIT;
+float smoothedValue = 0.0; // Exponential smoothing
 
-Adafruit_NeoPixel pixels(NUMPIXELS, NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800);
-int dir = 1;
-int bright = 0;
-// put function declarations here:
+OneButton btn = OneButton(
+  BUTTON_PIN,  // Input pin for the button
+  true,        // Button is active LOW
+  true         // Enable internal pull-up resistor
+);
+
 void setup() {
   Serial.begin(9600);
-  pixels.begin();
-  randomSeed(analogRead(0));
+
+  pinMode(ENVELOPE_PIN, INPUT); 
+
+  // Define the LED and put them to black
+  FastLED.addLeds<NEOPIXEL,NEOPIXEL_PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
+
+  // Put default HUE value
+  currentHue = HUE_INIT;
+
+  // Update the LED Strip
+  FastLED.show(); 
+
+  // Change the color when doing a simple click
+  btn.attachClick([](){
+    Serial.println("Change Color");
+    currentHue += HUE_CHANGE;
+    if(currentHue > 255)
+      currentHue = 0;
+  });
+
+  // Perform a long press to change the mode
+  btn.attachLongPressStart([]() {
+    Serial.println("Change Mode");
+    mode += 1;
+  });
+}
+void solidMode(){
+    for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(currentHue, SATURATION, 255/2);
+  }
+}
+
+void soundMode(){
+  // Read from sound sensor
+  int analogVal = analogRead(A0);
+
+  if(analogVal > MAX_VAL)
+    analogVal = MAX_VAL;
+
+  if(analogVal < MIN_VAL)
+    analogVal = MIN_VAL;
+
+  // Convert the value from analog input to 0-BRIGHTNESS
+  int val = map(analogVal, MIN_VAL, MAX_VAL, 0, BRIGHTNESS);
+
+  // Apply exponential smoothing
+  smoothedValue = ALPHA * val + (1.0 - ALPHA) * smoothedValue;
+
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CHSV(currentHue, SATURATION, smoothedValue);
+  }
 }
 
 void loop() {
-  pixels.clear();
-
-  for(int i=0; i<NUMPIXELS; i++) {
-
-  uint32_t blueFade = pixels.ColorHSV(43650,255,bright); //set blueFade color to variable bright
-  pixels.fill(blueFade);                                 //fill strip with color blueFade
-  pixels.show();                                         //send data to strip
-  bright = bright + dir;                                //change bright by +1 or -1
-  if(bright > 100 || bright < 1) dir = dir * -1;        //set dir to +1 or -1
-  delay(1000); 
-
-  int sensorValue = analogRead(1);
-  Serial.println(sensorValue);
+  switch (mode) {
+    case SOUND_MODE:
+      soundMode();
+      break;
+    case SOLID_MODE:
+      solidMode();
+      break;
+    case FADE_MODE:
+      // fadeMode();
+      break;
+    default:
+      // Reset to first mode
+      mode = 0;
+      break;
   }
+
+  // Update the LED Strip
+  FastLED.show();
+
+  // Refresh the button
+  btn.tick();
+
+  delay(10);
 }
